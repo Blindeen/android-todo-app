7305f5e8db3570d2914f4a37da7680ae3f5307cc
Blindeen <kuba.szmajda@gmail.com>
2024-06-02 23:09:24 +0200

    modify update method

--- app/src/main/java/com/project/todolist/db/dao/TaskDao.java
+++ app/src/main/java/com/project/todolist/db/dao/TaskDao.java
@@ -25,7 +25,7 @@ public interface TaskDao {
     Completable insertTask(String title, String description, String dateTime, long categoryId, boolean notification);
 
     @Update
-    void updateTasks(Task... tasks);
+    void updateTask(Task task);
 
     @Delete
     void delete(Task task);
