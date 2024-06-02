80b5df99e43459ebbba7b214bab2a34378867bab
Blindeen <kuba.szmajda@gmail.com>
2024-06-02 10:41:54 +0200

    validate add/edit activity form

--- app/src/main/java/com/project/todolist/activity/AddEditTaskActivity.java
+++ app/src/main/java/com/project/todolist/activity/AddEditTaskActivity.java
@@ -4,12 +4,14 @@ import static com.project.todolist.Utils.displayToast;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
@@ -18,7 +20,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.project.todolist.MainActivity;
 import com.project.todolist.R;
-import com.project.todolist.db.AppDatabase;
 import com.project.todolist.db.entity.Category;
 import com.project.todolist.db.entity.Task;
 
@@ -36,6 +37,9 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 public class AddEditTaskActivity extends MainActivity {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("MMM dd, yyyy hh:mm a");
 
+    private EditText titleInput, descriptionInput, dateTimeInput;
+    private Spinner categorySpinner;
+    private CheckBox notificationCheckbox;
     private Disposable addTaskQuerySubscriber;
 
     @Override
@@ -54,7 +58,7 @@ public class AddEditTaskActivity extends MainActivity {
             return insets;
         });
 
-        database = AppDatabase.getDatabase(this);
+        initializeWidgets();
         configureForm();
     }
 
@@ -71,32 +75,32 @@ public class AddEditTaskActivity extends MainActivity {
         }
     }
 
+    private void initializeWidgets() {
+        titleInput = findViewById(R.id.text_task_title);
+        descriptionInput = findViewById(R.id.text_task_description);
+        categorySpinner = findViewById(R.id.spinner_category);
+        dateTimeInput = findViewById(R.id.text_completion_date);
+        notificationCheckbox = findViewById(R.id.checkbox_notification);
+    }
+
     private void configureForm() {
         fetchCategories(this::setCategorySpinnerData);
         configTaskCompletionPicker();
     }
 
     private void setCategorySpinnerData(List<Category> categoryList) {
-        Spinner categorySpinner = findViewById(R.id.spinner_category);
-
         categoryList.add(0, new Category(0, ""));
         ArrayAdapter<Category> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, categoryList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(adapter);
-
-        if (chosenCategory != null) {
-            int selectedItem = categoryList.indexOf(new Category(chosenCategory, ""));
-            categorySpinner.setSelection(selectedItem);
-        }
     }
 
     private void configTaskCompletionPicker() {
-        EditText textCompletionDate = findViewById(R.id.text_completion_date);
-        textCompletionDate.setOnClickListener(v -> showDateTimePicker(textCompletionDate));
+        dateTimeInput.setOnClickListener(v -> showDateTimePicker());
     }
 
-    private void showDateTimePicker(EditText textCompletionDate) {
-        Calendar calendar = prepareCalendar(textCompletionDate.getText().toString());
+    private void showDateTimePicker() {
+        Calendar calendar = prepareCalendar(dateTimeInput.getText().toString());
         new DatePickerDialog(this, (view, year, month, dayOfMonth) -> {
             calendar.set(Calendar.YEAR, year);
             calendar.set(Calendar.MONTH, month);
@@ -107,7 +111,8 @@ public class AddEditTaskActivity extends MainActivity {
                 calendar.set(Calendar.MINUTE, minute);
 
                 String dateTimeString = LocalDateTime.ofInstant(calendar.toInstant(), calendar.getTimeZone().toZoneId()).format(DATE_TIME_FORMATTER);
-                textCompletionDate.setText(dateTimeString);
+                dateTimeInput.setText(dateTimeString);
+                dateTimeInput.setError(null);
             }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
         }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
@@ -134,26 +139,44 @@ public class AddEditTaskActivity extends MainActivity {
         }
     }
 
+    private boolean validateForm() {
+        boolean isValid = true;
+
+        TextView[] inputWidgets = {titleInput, descriptionInput, dateTimeInput};
+        for (TextView widget : inputWidgets) {
+            EditText etWidget = (EditText) widget;
+            if (etWidget.length() == 0) {
+                etWidget.setError("Field cannot be empty");
+                isValid = false;
+            }
+        }
+
+        Category chosenCategory = (Category) categorySpinner.getSelectedItem();
+        if (chosenCategory.getName().isEmpty()) {
+            TextView errorText = (TextView) categorySpinner.getSelectedView();
+            errorText.setError("");
+            errorText.setTextColor(Color.RED);
+            errorText.setText("Choose a category");
+        }
+
+        return isValid;
+    }
+
     private Task createTask() {
-        EditText titleInput = findViewById(R.id.text_task_title);
         String title = titleInput.getText().toString();
-
-        EditText descriptionInput = findViewById(R.id.text_task_description);
         String description = descriptionInput.getText().toString();
-
-        Spinner categorySpinner = findViewById(R.id.spinner_category);
         Category category = (Category) categorySpinner.getSelectedItem();
-
-        EditText dateTimeInput = findViewById(R.id.text_completion_date);
         String dateTime = dateTimeInput.getText().toString();
-
-        CheckBox notificationCheckbox = findViewById(R.id.checkbox_notification);
         boolean isChecked = notificationCheckbox.isChecked();
 
         return new Task(title, description, dateTime, isChecked, category.getCategoryId());
     }
 
     private void saveTask() {
+        if (!validateForm()) {
+            return;
+        }
+
         Task newTask = createTask();
         Completable completable = database.taskDao().insertTasks(newTask);
         addTaskQuerySubscriber = completable
