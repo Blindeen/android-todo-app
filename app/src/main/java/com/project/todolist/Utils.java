bb8394212ea05df497972f2b238bbc1be9e62f8f
Blindeen <kuba.szmajda@gmail.com>
2024-06-02 11:18:30 +0200

    add notification methods

--- app/src/main/java/com/project/todolist/Utils.java
+++ app/src/main/java/com/project/todolist/Utils.java
@@ -1,10 +1,52 @@
 package com.project.todolist;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
 
 public class Utils {
     public static void displayToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void createNotificationChannel(Context context) {
+        String channelId = context.getString(R.string.notification_channel_id);
+        CharSequence name = context.getString(R.string.notification_channel_name);
+        String description = context.getString(R.string.notification_channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+        NotificationChannel channel = new NotificationChannel(channelId, name, importance);
+        channel.setDescription(description);
+
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    private static Notification createNotification(Context context, String title, String message) {
+        String channelId = context.getString(R.string.notification_channel_id);
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        return new NotificationCompat.Builder(context, channelId)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+    }
+
+    public static void showNotification(Context context, String title, String message) {
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = createNotification(context, title, message);
+        mNotificationManager.notify(0, notification);
     }
 }
